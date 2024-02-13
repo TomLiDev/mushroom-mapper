620c3e4644ca71c917be4a7b31544e43f30213a9
TomLiDev <t.litchfield00@gmail.com>
2024-02-13 13:41:08 +0000

    Add 404, 403 and 505 error pages

--- mushroommapper/settings.py
+++ mushroommapper/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-tomlidev-mushroom-mapper-v6jmg0s734.us2.codeanyapp.com',
                  '8001-tomlidev-mushroom-mapper-v6jmg0s734.us2.codeanyapp.com',
